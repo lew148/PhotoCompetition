c103440f14b731eb4ddd484c71243295e22f5c3c
Lewis Reid <lewis.reid148@gmail.com>
2018-10-08 14:35:37 +0100

    Setup Finished

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = '18.202.128.247';
+var token = '892e1fac-76f9-47d9-8083-3fe7aaa3ae00';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
